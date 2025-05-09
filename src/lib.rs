f395ebd7c07cb6ada44d7c84e626953a9784329b
William Leader <83631101+wdlea@users.noreply.github.com>
2025-05-09 16:03:35 +1200

    Removed a clippy lint because it was causing unneccessary work

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 #![no_std]
-#![deny(
-    missing_docs,
-    clippy::empty_docs,
-    clippy::missing_docs_in_private_items
-)]
+#![deny(missing_docs, clippy::empty_docs)]
 
 //! A port of the (Pixy2 Library)[github.com/charmedlabs/pixy2/](Originally C++) to Rust.
 //! Originally licensed under the (GNU General Public License v2)[www.gnu.org/licenses/gpl-2.0.html].
